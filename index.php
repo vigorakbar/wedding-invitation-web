707e116fa446b96d0831a03ac728ed82575287ef
vigorakbar <vigorakbar@gmail.com>
2018-05-11 22:21:10 +0800

    add content structure

---
+++ index.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <title>Wedding Invitation</title>
+  </head>
+  <body>
+    <div class="segment">
+    	<div class="outer-content">
+    		<div class="middle-content">
+    			<div class="inner-content text-center">
+    				HEADER<br>
+    				HEADER<br>
+    				HEADER<br>
+    				HEADER<br>
+    				HEADER<br>
+    				HEADER<br>
+    				HEADER
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    <div id="time" class="segment">
+    	<div class="outer-content">
+    		<div class="middle-content">
+    			<div class="inner-content text-center">
+    				TIME<br>
+    				TIME<br>
+    				TIME<br>
+    				TIME<br>
+    				TIME<br>
+    				TIME<br>
+    				TIME
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    <div id="location" class="segment">
+    	<div class="outer-content">
+    		<div class="middle-content">
+    			<div class="inner-content text-center">
+    				LOCATION<br>
+    				LOCATION<br>
+    				LOCATION<br>
+    				LOCATION<br>
+    				LOCATION<br>
+    				LOCATION<br>
+    				LOCATION
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    <div id="rsvp" class="segment">
+    	<div class="outer-content">
+    		<div class="middle-content">
+    			<div class="inner-content text-center">
+    				RSVP<br>
+    				RSVP<br>
+    				RSVP<br>
+    				RSVP<br>
+    				RSVP<br>
+    				RSVP<br>
+    				RSVP
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    <div id="prayer" class="segment">
+    	<div class="outer-content">
+    		<div class="middle-content">
+    			<div class="inner-content text-center">
+    				PRAYER(?)<br>
+    				PRAYER(?)<br>
+    				PRAYER(?)<br>
+    				PRAYER(?)<br>
+    				PRAYER(?)<br>
+    				PRAYER(?)<br>
+    				PRAYER(?)
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>
